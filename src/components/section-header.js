d01aa25883c56390f260cd3ce4655484474f490d
Balazs <balazs@otakomaiya.com>
2020-08-18 22:06:24 +0100

    Max width section header

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -5,7 +5,9 @@ import { COLORS } from "../styles/constants"
 const SectionHeader = ({ title, description }) => (
   <>
     <h2>{title}</h2>
-    <p style={{ color: COLORS.mediumGray }}>{description}</p>
+    <p style={{ color: COLORS.mediumGray, maxWidth: 640, margin: '0 auto', marginBottom: '1.5rem', lineHeight: 1.5 }}>
+        {description}
+    </p>
   </>
 )
 
