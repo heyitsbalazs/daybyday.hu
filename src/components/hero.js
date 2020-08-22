2589680011ecffd354cc5319d0e78f29cad9e82e
Balazs <balazs@otakomaiya.com>
2020-08-22 18:41:40 +0100

    Installed TailwindCSS, Fake iOS / Android download buttons

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import Button from '../components/button'
 import headerImage from '../images/header.png'
 import Logo from '../images/logo.svg'
+import AppStore from '../images/app-store.svg'
+import PlayStore from '../images/play-store.svg'
 
 const Header = ({ siteTitle }) => (
     <div
@@ -27,11 +27,17 @@ const Header = ({ siteTitle }) => (
         />
         <img src={ Logo } alt={ 'logo' } />
         <p style={ { textAlign: 'center', maxWidth: 540 } }>
-            Exkluziv ajanlatok kedvenc markaidtol, nap mint nap.
+            Exkluziv ajanlatok kedvenc markaidtol, nap mint nap
             <br />
-            <b>Csak neked.</b>
+            <h2 className="heading heading--large text-center mt-4">
+                <span className="heading__elem m-auto">
+                    <strong className='lg:text-lg'>CSAK NEKED.</strong>
+                </span>
+            </h2>
         </p>
-        <Button>Értesítést kérek!</Button>
+
+        <img className='mt-8 h-12' src={ AppStore } alt={'Letoltheto az App Store-bol'} />
+        <img style={ { height: '3.3rem' } } src={ PlayStore } alt={'Letoltheto a Play Store-bol'} />
     </div>
 )
 
