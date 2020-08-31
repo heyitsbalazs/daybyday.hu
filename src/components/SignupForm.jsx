6f14f38baf78240e4812632c750227f68bb19e7a
Balazs <balazs@otakomaiya.com>
2020-08-31 11:33:38 +0100

    Started signup form

---
+++ src/components/SignupForm.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { Transition } from '@tailwindui/react';
+import axios from 'axios';
+
+// eslint-disable-next-line react/prop-types
+function SignupForm({ show, close }) {
+  const [email, setEmail] = useState('');
+
+  const submit = () => {
+    // e.preventDefault();
+
+    axios
+      .post('https://www.getwaitlist.com/waitlist', {
+        email,
+        api_key: 'GQZDWZ',
+        referral_link: 'https://daybyday.hu',
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  };
+
+  return (
+    <Transition
+      show={show}
+      enter="ease-out duration-300"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="ease-in duration-200"
+      leaveTo="opacity-0"
+      leaveFrom="opacity-100"
+    >
+      <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div className="fixed inset-0 transition-opacity">
+            <div className="absolute inset-0 bg-gray-500 opacity-75" />
+          </div>
+
+          <div
+            className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="modal-headline"
+          >
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="sm:flex sm:items-start">
+                <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-orange-100 sm:mx-0 sm:h-10 sm:w-10">
+                  🎉
+                </div>
+                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                    Hamarosan elindulunk!
+                  </h3>
+                  <div className="mt-2">
+                    <p className="text-sm leading-5 text-gray-500">
+                      {/* eslint-disable-next-line max-len */ }
+                      Szolgaltatasunk hamarosan elindul, es exkluziv, szemelyre szabott ajanlatokkal fogjuk napjaidat bearanyozni –
+                      { ' ' }
+                      <b>mert megerdemled.</b>
+                    </p>
+
+                    <p className="text-sm leading-5 text-gray-500 mt-4">
+                      {/* eslint-disable-next-line max-len */ }
+                      Csatlakozz a varolistankoz, es legy az elsok kozott, akik kiprobaljak a DAYBYDAY-t.
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <form onSubmit={() => submit()}>
+                      <input
+                        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                        type="email"
+                        placeholder="neved@email.hu"
+                        onChange={(event) => setEmail(event.target.value)}
+                        required
+                      />
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-100 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+                <button
+                  onClick={() => submit()}
+                  type="button"
+                  className="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                >
+                  Csatlakozom
+                </button>
+              </span>
+              <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+                <button
+                  onClick={() => close()}
+                  type="button"
+                  className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                >
+                  Bezaras
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  );
+}
+
+export default SignupForm;
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,95 +1,98 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import headerImage from '../images/header.png'
-import Logo from '../images/logo.svg'
-import amplitude from 'amplitude-js'
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import amplitude from 'amplitude-js';
+import headerImage from '../images/header.png';
+import Logo from '../images/logo.svg';
 import SignupForm from './SignupForm';
 import AppStore from '../images/app-store.svg';
 import PlayStore from '../images/play-store.svg';
 
 amplitude.getInstance().init('629f4c710cc7cd13b84f305564970aa4');
 
-const Header = ({ siteTitle }) => {
-    const [hasClicked, setHasClicked] = useState(false);
-    const [showForm, setShowForm] = useState(false);
+const Header = () => {
+  const [hasClicked, setHasClicked] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
-    useEffect(() => {
-        amplitude.logEvent('PageView')
-    }, [])
+  useEffect(() => {
+    amplitude.logEvent('PageView');
+  }, []);
 
-    const onDownload = (type) => {
-        if (!hasClicked) {
-            amplitude.logEvent('AppDownload', { type })
-        }
+  const onDownload = (type) => {
+    console.log('hello');
 
-        setHasClicked(true);
-        setShowForm(true);
+    if (!hasClicked) {
+      amplitude.logEvent('AppDownload', { type });
     }
 
-    return (
-        <Fragment>
-            <div
-                style={ {
-                    display: 'flex',
-                    alignItems: 'center',
-                    flexDirection: 'column',
-                    padding: '4rem 1rem',
-                } }
-            >
-                <div
-                    style={ {
-                        backgroundImage: `url(${ headerImage })`,
-                        position: 'absolute',
-                        top: 0,
-                        zIndex: -5,
-                        height: '100vh',
-                        width: '100vw',
-                        opacity: 0.5,
-                    } }
-                />
-                <img src={ Logo } alt={ 'logo' } />
-                <p style={ { textAlign: 'center', maxWidth: 540 } }>
-                    Exkluziv ajanlatok kedvenc markaidtol, nap mint nap
-                    <br />
-                    <span className="heading heading--large text-center mt-4">
-                        <span className="heading__elem m-auto">
-                            <strong className='lg:text-lg'>CSAK NEKED.</strong>
-                        </span>
-                    </span>
-                </p>
+    setHasClicked(true);
+    setShowForm(true);
+  };
 
-                { showForm
-                    ? <SignupForm />
-                    : <Fragment>
-                        <img
-                            onClick={ () => {
-                                onDownload('App Store')
-                            } }
-                            className='mt-8 h-12'
-                            src={ AppStore }
-                            alt={ 'Letoltheto az App Store-bol' }
-                        />
-                        <img
-                            onClick={ () => {
-                                onDownload('Play Store')
-                            } }
-                            style={ { height: '3.3rem' } }
-                            src={ PlayStore }
-                            alt={ 'Letoltheto a Play Store-bol' }
-                        />
-                    </Fragment>
-                }
-            </div>
-        </Fragment>
-    );
+  const closeSignupForm = () => {
+    setShowForm(false);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          padding: '4rem 1rem',
+        }}
+      >
+        <div
+          style={{
+            backgroundImage: `url(${headerImage})`,
+            position: 'absolute',
+            top: 0,
+            zIndex: -5,
+            height: '100vh',
+            width: '100vw',
+            opacity: 0.5,
+          }}
+        />
+        <img src={Logo} alt="logo" />
+        <p style={{ textAlign: 'center', maxWidth: 540 }}>
+          Exkluziv ajanlatok kedvenc markaidtol, nap mint nap
+          <br />
+          <span className="heading heading--large text-center mt-4">
+            <span className="heading__elem m-auto">
+              <strong className="lg:text-lg">CSAK NEKED.</strong>
+            </span>
+          </span>
+        </p>
+
+        <img
+          onClick={() => {
+            onDownload('App Store');
+          }}
+          className="mt-8 h-12"
+          src={AppStore}
+          alt="Letoltheto az App Store-bol"
+        />
+        <img
+          onClick={() => {
+            onDownload('Play Store');
+          }}
+          style={{ height: '3.3rem' }}
+          src={PlayStore}
+          alt="Letoltheto a Play Store-bol"
+        />
+
+        <SignupForm close={closeSignupForm} show={showForm} />
+      </div>
+    </>
+  );
 };
 
 Header.propTypes = {
-    siteTitle: PropTypes.string,
-}
+  siteTitle: PropTypes.string,
+};
 
 Header.defaultProps = {
-    siteTitle: ``,
-}
+  siteTitle: '',
+};
 
-export default Header
+export default Header;
