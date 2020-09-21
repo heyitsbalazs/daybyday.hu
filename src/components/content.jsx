b001567b8565dbe7f6d21170caf970b20f648111
Balazs <balazs@otakomaiya.com>
2020-09-21 21:54:42 +0100

    Installed fontawesome

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
 
 const Content = () => (
   <div className="bg-white">
@@ -19,18 +21,11 @@ const Content = () => (
             <div className="flex">
               <div className="flex-shrink-0">
                 <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-                    />
-                  </svg>
+                  <FontAwesomeIcon icon={faLightbulb} />
                 </div>
               </div>
               <div className="ml-4">
-                <h4 className="text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</h4>
+                <h4 className="text-lg leading-6 font-medium text-gray-900">Mesterseges Intelligencia</h4>
                 <p className="mt-2 text-base leading-6 text-gray-500">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
                 </p>
